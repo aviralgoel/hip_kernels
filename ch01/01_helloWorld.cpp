9864ca24cf59329f6421814e2792718f1051e2b1
Aviral Goel <aviral.goel@amd.com>
2025-02-04 16:39:20 -0500

    testing with hello world

--- ch01/01_helloWorld.cpp
+++ ch01/01_helloWorld.cpp
@@ -1,6 +1,16 @@
 #include <hip/hip_runtime.h>
 #include <iostream>
 
+#define HIP_CHECK(expression)                                                  \
+  {                                                                            \
+    const hipError_t status = expression;                                      \
+    if (status != hipSuccess) {                                                \
+      std::cerr << "HIP error " << status << ": " << hipGetErrorString(status) \
+                << " at " << __FILE__ << ":" << __LINE__ << std::endl;         \
+      exit(-1);                                                                \
+    }                                                                          \
+  }
+
 __global__ void helloWorldKernel() {
   int globalThreadId = blockIdx.x * blockDim.x + threadIdx.x;
   int blockId = blockIdx.x;
@@ -16,12 +26,15 @@ int main() {
   int numBlocks = 2;       // number of blocks per grid
   int threadsPerBlock = 4; // number of threads per block
 
-  // Launch the kernel on GPU
-  hipLaunchKernelGGL(helloWorldKernel, dim3(numBlocks), dim3(threadsPerBlock),
+  //hipLaunchKernelGGL(helloWorldKernel, dim3(numBlocks), dim3(threadsPerBlock),
                      0, 0);
 
+  // Launch the kernel on GPU
+  HIP_CHECK(hipLaunchKernelGGL(helloWorldKernel, dim3(numBlocks), dim3(threadsPerBlock),
+                     0, 0));
+
   // wait for GPU to finish the work
-  hipDeviceSynchronize();
+  HIP_CHECK(hipDeviceSynchronize());
 
   return 0;
 }
